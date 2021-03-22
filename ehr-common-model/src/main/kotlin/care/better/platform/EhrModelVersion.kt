0ed82e2b0f38972cded6553aa557a3c42db5a0eb
Primoz Delopst <primoz.delopst@marand.si>
2021-03-22 14:22:02 +0100

    Cosmetics

--- ehr-common-model/src/main/kotlin/care/better/platform/EhrModelVersion.kt
+++ ehr-common-model/src/main/kotlin/care/better/platform/EhrModelVersion.kt
@@ -46,6 +46,4 @@ class EhrModelVersion(vararg versions: Version<*>?) : ModelVersion(*versions) {
     fun getRmVersion(): RmVersion? = getVersion(RmVersion::class.java)
 
     fun getTpVersion(): TpVersion? = getVersion(TpVersion::class.java)
-
-    override fun getIdentifier(): String = "EHR_MODEL_VERSION"
 }
