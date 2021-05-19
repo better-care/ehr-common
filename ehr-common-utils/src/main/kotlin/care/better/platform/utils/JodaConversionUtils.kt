264488b1fb14c163fa892cfd2f4b7e98256e3a7a
Dušan Markovič <dusan.markovic@better.care>
2021-05-19 10:36:09 +0200

    THINKEHR-5599 - address reviews EC-CR-4 concerns no.3

--- ehr-common-utils/src/main/kotlin/care/better/platform/utils/JodaConversionUtils.kt
+++ ehr-common-utils/src/main/kotlin/care/better/platform/utils/JodaConversionUtils.kt
@@ -71,7 +71,7 @@ class JodaConversionUtils {
         fun toPeriod(durationValue: String): Period {
             val (value, negative) =
                 if (durationValue.startsWith("-P")) {
-                    Pair(durationValue.replaceFirst("-P", "P"), true)
+                    Pair(durationValue.substring(1), true)
                 } else {
                     Pair(durationValue, false)
                 }
