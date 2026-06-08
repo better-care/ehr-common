better-care/ehr-common

commit 6f85b1d035901491672e294f50e54c81aa225de2

Add support for RM aware Kotlin serialization for JSON format

ehr-common-model/src/main/kotlin/org/openehr/am/aom/CComplexObject.kt
@@ -15,6 +15,7 @@
 
 package org.openehr.am.aom
 
+import care.better.platform.annotation.Open
 import javax.xml.bind.annotation.*
 
 
@@ -25,6 +26,7 @@ import javax.xml.bind.annotation.*
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "C_COMPLEX_OBJECT", propOrder = ["attributes"])
 @XmlSeeAlso(value = [CArchetypeRoot::class, TComplexObject::class])
+@Open
 open class CComplexObject : CDefinedObject() {
     companion object {
         private const val serialVersionUID: Long = 0L

ehr-common-model/src/main/kotlin/org/openehr/am/aom/CDefinedObject.kt
@@ -15,6 +15,7 @@
 
 package org.openehr.am.aom
 
+import care.better.platform.annotation.Open
 import javax.xml.bind.annotation.XmlAccessType
 import javax.xml.bind.annotation.XmlAccessorType
 import javax.xml.bind.annotation.XmlSeeAlso
@@ -27,7 +28,8 @@ import javax.xml.bind.annotation.XmlType
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "C_DEFINED_OBJECT")
 @XmlSeeAlso(value = [CPrimitiveObject::class, CComplexObject::class, CDomainType::class])
-open class CDefinedObject : CObject() {
+@Open
+class CDefinedObject : CObject() {
     companion object {
         private const val serialVersionUID: Long = 0L
     }

ehr-common-model/src/main/kotlin/org/openehr/rm/datatypes/DvText.kt
@@ -17,6 +17,9 @@ package org.openehr.rm.datatypes
 
 import care.better.platform.annotation.Open
 import care.better.platform.annotation.Required
+import kotlinx.serialization.Polymorphic
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import java.util.*
 import javax.xml.bind.annotation.*
 
@@ -33,9 +36,15 @@ import javax.xml.bind.annotation.*
         "mappings",
         "language",
         "encoding"])
-@XmlSeeAlso(DvCodedText::class)
+@Serializable
+@SerialName("DV_TEXT")
+@Polymorphic
 @Open
 class DvText() : DataValue() {
+    companion object {
+        private const val serialVersionUID: Long = 0L
+    }
+
     @JvmOverloads
     constructor(
             value: String,
@@ -50,10 +59,6 @@ class DvText() : DataValue() {
         this.mappings = mappings
         this.language = language
         this.encoding = encoding
-    }
-
-    companion object {
-        private const val serialVersionUID: Long = 0L
     }
 
     @XmlElement(required = true)

commit 70a2f337d1a48b7e485fbbf96b52e85772c060f9

Fix de/serialization problems introduced with 6f85b1d035901491672e294f50e54c81aa225de2

ehr-common-model/src/main/kotlin/org/openehr/am/aom/CComplexObject.kt
@@ -15,7 +15,6 @@
 
 package org.openehr.am.aom
 
-import care.better.platform.annotation.Open
 import javax.xml.bind.annotation.*
 
 
@@ -26,7 +25,6 @@ import javax.xml.bind.annotation.*
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "C_COMPLEX_OBJECT", propOrder = ["attributes"])
 @XmlSeeAlso(value = [CArchetypeRoot::class, TComplexObject::class])
-@Open
 open class CComplexObject : CDefinedObject() {
     companion object {
         private const val serialVersionUID: Long = 0L

ehr-common-model/src/main/kotlin/org/openehr/am/aom/CDefinedObject.kt
@@ -15,7 +15,6 @@
 
 package org.openehr.am.aom
 
-import care.better.platform.annotation.Open
 import javax.xml.bind.annotation.XmlAccessType
 import javax.xml.bind.annotation.XmlAccessorType
 import javax.xml.bind.annotation.XmlSeeAlso
@@ -28,8 +27,7 @@ import javax.xml.bind.annotation.XmlType
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "C_DEFINED_OBJECT")
 @XmlSeeAlso(value = [CPrimitiveObject::class, CComplexObject::class, CDomainType::class])
-@Open
-class CDefinedObject : CObject() {
+open class CDefinedObject : CObject() {
     companion object {
         private const val serialVersionUID: Long = 0L
     }

ehr-common-model/src/main/kotlin/org/openehr/rm/datatypes/DvText.kt
@@ -43,10 +43,6 @@ import javax.xml.bind.annotation.*
 @Polymorphic
 @Open
 class DvText() : DataValue() {
-    companion object {
-        @Suppress("unused")
-        private const val serialVersionUID: Long = 0L
-    }
 
     @JvmOverloads
     constructor(